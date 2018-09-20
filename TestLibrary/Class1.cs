00eb74cdf12e0faec7067e88ec222d5192da9dc0
Prystaiko <pristayko.pavlo@gmail.com>
2018-09-20 17:50:41 +0300

    Add project files.

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class ClassWishListTest
+    {
+        string password = "12345";
+        string login = "pristayko.pavlo@gmail.com";
+
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Close();
+        }
+
+        [Test]
+        public void AddItemToCart()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/login");
+
+            driver.FindElement(By.Id("input-email")).SendKeys(login);
+            driver.FindElement(By.Id("input-password")).SendKeys(password);
+
+
+            driver.FindElement(By.ClassName("dropdown-toggle")).FindElement(By.XPath("//div[text() = 'TextToFind']")) .Click();
+            Thread.Sleep(2000);
+        }
+    }
+}
