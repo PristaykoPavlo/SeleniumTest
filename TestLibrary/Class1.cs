0a467273dc5065f07a3e24e31c2b94795e02ea4a
Pavlo <pristayko.pavlo@gmail.com>
2018-09-24 02:05:24 +0300

    new

--- TestLibrary/Class1.cs
+++ TestLibrary/Class1.cs
@@ -8,50 +8,89 @@ using NUnit;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace TestLibrary
 {
 
     public class ClassWishListTest
     {
-        string password = "12345";
-        string login = "pristayko.pavlo@gmail.com";
-
+        readonly string password = "12345";
+        readonly string login = "pristayko.pavlo@gmail.com";
+        readonly string productName = "MacBook";
         private IWebDriver driver;
 
         [OneTimeSetUp]
         public void BeforeAllMethods()
         {
             driver = new ChromeDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
         }
-        
+
         [OneTimeTearDown]
         public void AfterAllMethods()
         {
             driver.Close();
         }
 
+        [TearDown]
+        public void Logout()
+        {
+            WebDriverWait wait;
+            IWebElement element;
+
+            /*Clear Cart*/
+            driver.FindElement(By.XPath("//button[contains(@class,'btn btn-inverse btn-block btn-lg dropdown-toggle')]")).Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            element = wait.Until(driver => driver.FindElement(By.XPath("//button[contains(@class,'btn btn-danger btn-xs')]")));
+            element.Click();
+
+
+            /*Logout*/
+            driver.FindElement(By.XPath("//li[contains(@class,'dropdown')]//a[contains(@class,'dropdown-toggle')]")).Click();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            element = wait.Until(driver => driver.FindElement(By.XPath("//ul[contains(@class,'dropdown-menu dropdown-menu-right')]//a[contains(text(),'Logout')]")));
+            element.Click();
+
+        }
+
         [Test]
         public void AddItemToCart()
         {
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/login");
+            Login(login, password);
 
-            driver.FindElement(By.Id("input-email")).SendKeys(login);
-            driver.FindElement(By.Id("input-password")).SendKeys(password);
-            driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
-            Thread.Sleep(1000);
+            driver.FindElement(By.Id("logo")).Click();//go to main page
 
-            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=common/home");
+            /*Add new item to wishlist*/
+            driver.FindElement(By.XPath(String.Format("//div[contains(@class, 'product-layout')]//a[contains(text(),'{0}')]/../../following-sibling::div/button[contains(@data-original-title,'Wish')]", productName))).Click();
             
-            driver.FindElement(By.XPath("//button[contains(@data-toggle,'tooltip')]")).Click();
+            /*Add item from wishlist to cart*/
+            driver.FindElement(By.Id("wishlist-total")).Click();
+            driver.FindElement(By.XPath("//td[contains(@class,'text-right')]//button[contains(@class,'btn btn-primary')]")).Click();
 
 
-            
-            Thread.Sleep(10000);
+            string actual = driver.FindElement(By.Id("cart-total")).Text;
+            string expected = driver.FindElement(By.XPath("//td[contains(@class,'text-right')]//div[contains(@class,'price')]")).Text;
+
+            string[] price = actual.Split('-');
+            Console.WriteLine(price[1]);
+
+            //Assert.AreEqual(expected,actual);
         }
 
+        private void Login(string log, string pass)
+        {
+
+            driver.Navigate().GoToUrl("http://atqc-shop.epizy.com/index.php?route=account/login");
+
+            driver.FindElement(By.Id("input-email")).SendKeys(log);
+            driver.FindElement(By.Id("input-password")).SendKeys(pass);
+            driver.FindElement(By.CssSelector("input.btn.btn-primary")).Click();
+
+
+
+        }
     }
 }
